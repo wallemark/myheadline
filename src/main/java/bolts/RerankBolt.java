81becac8da562234c7f91f7cfd17ff736fe64197
ryanyycao <ryanyycao@tencent.com>
2016-07-12 16:35:58 +0800

    rerank

--- src/main/java/bolts/RerankBolt.java
+++ src/main/java/bolts/RerankBolt.java
@@ -5,8 +5,10 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Tuple;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Arrays;
 
 /**
  * Created by Administrator on 2016/7/6.
@@ -20,7 +22,13 @@ public class RerankBolt extends BaseBasicBolt {
         int uin = input.getIntegerByField("uin");
         System.out.println(uin);
         List<OfflineResult> offlineresult = (LinkedList<OfflineResult>)input.getValueByField("Filter_id_topic");
-        for(OfflineResult x:offlineresult){
+        OfflineResult[] res = new OfflineResult[offlineresult.size()];
+        for(int i=0;i<res.length;i++){
+            res[i] = offlineresult.get(i);
+        }
+        Arrays.sort(res, new MyComprator());
+
+        for(OfflineResult x:res){
             System.out.print(x.getid()+"     ");
             System.out.print(x.getscore()+"     ");
             System.out.print(x.gettitle()+"     ");
@@ -31,5 +39,14 @@ public class RerankBolt extends BaseBasicBolt {
     }
 
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    }
+    class MyComprator implements Comparator {
+        public int compare(Object arg0, Object arg1) {
+            if(((OfflineResult)arg0).getscore()>((OfflineResult)arg1).getscore()){
+                return -1;
+            }else{
+                return 1;
+            }
+        }
     }
 }
